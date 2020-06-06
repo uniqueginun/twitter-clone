a7ddf33ad2b3d02fcc0ddb7459ea5c41ac38d678
Othman Mohammed <uniqueginun@gmail.com>
2020-06-06 06:53:36 +0300

    styling-the-base-template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    
-                    <example-component />
-
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="flex">
+    <div class="w-3/12">nav</div>
+    <div class="w-7/12 border border-t-0 border-b-0 border-gray-800">timeline</div>
 </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      'md': '640px',
+      'lg': '768px',
+      'xl': '1024px'
+    },
     extend: {},
   },
   variants: {},
