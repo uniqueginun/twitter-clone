501e09d414365479a051a9d8dd137bcef39caa32
Othman Mohammed <uniqueginun@gmail.com>
2020-06-06 04:13:27 +0300

    fresh laravel installation with sanctum installed

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        {{ user }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                user: null
+            }
+        },
+        methods: {
+            async getUser() {
+                const {data} = await axios.get('/api/user');
+                console.log(data);
+                this.user = data;
+            }
+        },
+        mounted() {
+            this.getUser();
+        }
+    }
+</script>
