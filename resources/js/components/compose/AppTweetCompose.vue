3ff24ad062b3d487146cdef4dd3b250c4f9c7218
Othman Mohammed <uniqueginun@gmail.com>
2020-06-08 09:42:13 +0300

    automatically-adjusting-the-textarea-height

--- resources/js/components/compose/AppTweetCompose.vue
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -4,11 +4,9 @@
             <img :src="$user.avatar" class="w-12 rounded-full" />
         </div>
         <div class="flex-grow">
-            <textarea class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2"
-                      placeholder="what's happening?"
-                      autofocus
-                      v-model="form.body"
-            ></textarea>
+            <app-tweet-compose-textarea
+                v-model="form.body"
+            />
             <div class="flex justify-between">
                 <div>Actions</div>
                 <div>
@@ -22,7 +20,6 @@
 <script>
     export default {
         name: "AppTweetCompose",
-
         data: () => {
             return {
                 form: {
---
+++ resources/js/components/compose/AppTweetComposeTextarea.vue
@@ -0,0 +1,34 @@
+<template>
+    <textarea
+            ref="composer"
+            class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2"
+            placeholder="what's happening?"
+            autofocus
+            @input="$emit('input', $event.target.value); resize($event);"
+            :value="value"
+    ></textarea>
+</template>
+
+<script>
+    export default {
+        name: "AppTweetComposeTextarea",
+
+        props: {
+            value: {
+                type: String,
+                required: false
+            }
+        },
+
+        methods: {
+            resize(e) {
+                this.$refs.composer.style.height = 'auto';
+                this.$refs.composer.style.height = `${e.target.scrollHeight}px`;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
